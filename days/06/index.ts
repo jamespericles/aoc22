9342e7b69f7c65fb1fa127cf9424507dab79dff8
James Pericles <jamesspericles@gmail.com>
2022-12-07 16:24:55 -0600

    finishde day 6

--- days/06/index.ts
+++ days/06/index.ts
@@ -3,16 +3,28 @@
 const fs = require('fs')
 
 let input: string[]
-input = fs.readFileSync('test.txt', { encoding: 'utf-8' }).split('')
+input = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('')
+
+let slidingWindow: string[] = []
+let marker: number = 0
+
+// Helper function to determine if there exists any duplicate elements
+// Sets cannot contain duplicates, so this function will return true if the newly created Set and the array it was passed do not have the same length
+const hasDuplicates = (arr: string[]) =>
+  arr.length !== new Set<string>(arr).size
 
 for (let i: number = 0; i < input.length; i++) {
-  let window: string[] = []
-  window.push(input[i], input[i + 1], input[i + 2], input[i + 3])
+  // Populate our window
+  if (slidingWindow.length !== 4) {
+    slidingWindow.push(input[i])
+    marker++
+  }
 
-  if (window.length === 4) {
-    let compare: string = window.shift()!
-
-    if (window.includes(compare)) {
-    }
+  if (hasDuplicates(slidingWindow)) {
+    // This slides our window down the array once more and increments our marker
+    // The marker will stop incrementing once we stop finding duplicates, which will represent the first instance of four unique elements in our input array
+    slidingWindow.shift()
   }
 }
+
+console.log(marker)
